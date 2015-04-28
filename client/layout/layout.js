f22322cdfd39ebc143153169c97ba62f94db2b38
Julien Mayer <jalexismayer@gmail.com>
2015-04-28 15:04:48 -0700

    add simulate friends js

--- client/layout/layout.js
+++ client/layout/layout.js
@@ -1,14 +1,5 @@
 Template.layout.helpers({
-  dummy: function(){
-    return Meteor.userId() == "EGy2KuTmz7gn9bx5g";
-  }
 });
 
 Template.layout.events({
-  'click .simulate': function(){
-    Meteor.call("simulateFriends", function(error, result){
-      console.log('Error: ' + error);
-      console.log('Result: ' + result);
-    });
-  }
 })
