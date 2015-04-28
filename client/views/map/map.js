c00d8418dfbf4f818d97b26fb5d5b2dc210ddccf
Julien Mayer <jalexismayer@gmail.com>
2015-04-27 18:34:10 -0700

    add simulate button

--- client/views/map/map.js
+++ client/views/map/map.js
@@ -1,12 +1,7 @@
 Template.map.onRendered(function(){
-  
+
 });
 
 Template.map.events({
-  'click button': function(){
-    Meteor.call("simulateFriends", function(error, result){
-      console.log('Error: ' + error);
-      console.log('Result: ' + result);
-    });
-  }
+  
 })
