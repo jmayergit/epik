2eee7e63679668e0ab58a865d60528601d763cac
Julien Mayer <jalexismayer@gmail.com>
2015-05-10 14:16:59 -0700

    refactor image html and js

--- client/views/markers/images/show.js
+++ client/views/markers/images/show.js
@@ -1,11 +1,11 @@
 Template.markerimageshow.helpers({
-  images: function() {
+  image: function() {
     // find another way to organize data
     var href = window.location.href;
     var reID = /[a-zA-Z0-9]{15,25}/;
     var marker_id = href.match(reID)[0];
     var marker = MarkersCollection.find({_id: marker_id});
     var photo_id = marker.fetch()[0].photo_id;
-    return Images.find({_id: photo_id});
+    return Images.findOne({_id: photo_id});
   }
 });
